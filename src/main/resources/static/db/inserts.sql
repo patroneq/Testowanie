36688b67291cfe7646735b2dd4d97efd94c51128
Patryk Tomczyk <patroneq@gmail.com>
2017-11-23 10:49:03 +0100

    Dodanie przykladowych insertow do inserts.sql

---
+++ src/main/resources/static/db/inserts.sql
@@ -0,0 +1,35 @@
+INSERT INTO series VALUES (1, 'Gra o tron');
+INSERT INTO series VALUES (2, 'House of cards');
+INSERT INTO series VALUES (3, 'The walking dead');
+
+INSERT INTO season VALUES (1, 1, 1);
+INSERT INTO season VALUES (2, 2, 1);
+INSERT INTO season VALUES (3, 3, 1);
+INSERT INTO season VALUES (4, 4, 1);
+INSERT INTO season VALUES (5, 5, 1);
+INSERT INTO season VALUES (6, 6, 1);
+INSERT INTO season VALUES (7, 7, 1);
+INSERT INTO season VALUES (8, 1, 2);
+INSERT INTO season VALUES (9, 2, 2);
+INSERT INTO season VALUES (10, 3, 2);
+INSERT INTO season VALUES (11, 4, 2);
+INSERT INTO season VALUES (12, 1, 3);
+INSERT INTO season VALUES (13, 2, 3);
+INSERT INTO season VALUES (14, 3, 3);
+INSERT INTO season VALUES (15, 4, 3);
+
+INSERT INTO episode VALUES (1, 1, 'GOT - S01E01', 1);
+INSERT INTO episode VALUES (2, 2, 'GOT - S01E02', 1);
+INSERT INTO episode VALUES (3, 3, 'GOT - S01E03', 1);
+INSERT INTO episode VALUES (4, 4, 'GOT - S01E04', 1);
+INSERT INTO episode VALUES (5, 1, 'GOT - S02E01', 2);
+INSERT INTO episode VALUES (6, 2, 'GOT - S02E02', 2);
+INSERT INTO episode VALUES (7, 3, 'GOT - S02E03', 2);
+INSERT INTO episode VALUES (8, 4, 'GOT - S02E04', 2);
+INSERT INTO episode VALUES (9, 5, 'GOT - S02E05', 2);
+INSERT INTO episode VALUES (10, 1, 'HOC - S01E01', 8);
+INSERT INTO episode VALUES (11, 2, 'HOC - S01E02', 8);
+INSERT INTO episode VALUES (12, 1, 'HOC - S02E01', 9);
+
+INSERT INTO movie VALUES (1, 'Titanic');
+INSERT INTO movie VALUES (2, 'Predator');
